8729a4cf1917d5f128da8dec2c852394286b6f54
randolphjr7 <randolphjr7@gmail.com>
2020-06-25 21:13:46 -0400

    comparator

--- com/company/JukeBox1.java
+++ com/company/JukeBox1.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 
 
 public class JukeBox1 {
@@ -29,21 +30,60 @@ public class JukeBox1 {
         }
     }
 
+    // inner class that implements Comparator
+    // type parameter matches the type we're going to compare
+    class BpmCompare implements Comparator<Song> {
+        // compare beats per minute
+        public int compare(Song one, Song two) {
+            int oneBpm = Integer.parseInt(one.bpm);
+            int twoBpm = Integer.parseInt(two.bpm);
+
+            if(oneBpm < twoBpm)
+                return 1;
+            if(oneBpm > twoBpm)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
 
 
     // load the file & print song list
     public void go() {
+
         getSongs();
+
+        // before any sorting
+        System.out.println("\nSonglist without any sorting:");
+        System.out.println(songList);
+
+        // sorts songs by title
+        System.out.println("\nSonglist sorted by Title");
         Collections.sort(songList);
         System.out.println(songList);
 
-        // instance of the Comprartor inner class
+        // instance of the Comparator inner class
         ArtistCompare artistCompare  = new ArtistCompare();
         // if you pass a Comparator to the sort method, the sort order is determined by the Comparator
         // rather than the element's own compareTo() method.
+        System.out.println("\nSonglist sorted by Artist");
         Collections.sort(songList, artistCompare);
-
         System.out.println(songList);
+
+        // instance of Bpm Comparator inner class
+        BpmCompare bpmCompare = new BpmCompare();
+        System.out.println("\nSonglist sorted by BPM");
+        Collections.sort(songList, bpmCompare);
+        System.out.println(songList);
+
+        // SET - Collections that do no allow duplicates
+        HashSet<Song> songSet = new HashSet<Song>(); // a new HashSet parameterized to hold songs
+        // HashSet has a simple addAll() method that can take another collection and use it to populate
+        // the HashSet. It's the same as if we added each song one at a time (except much simpler)
+        songSet.addAll(songList);
+        //System.out.println("\nHashSet Does Not Allow Duplicates");
+        //System.out.println(songList);
     }
 
     // read the file & call the addSong() method for each line
--- com/company/Song.java
+++ com/company/Song.java
@@ -31,7 +31,7 @@ public class Song implements Comparable<Song> {
     }
 
     public String toString() {
-        return title + ": " + artist;
+        return title;
     }
 
     @Override
